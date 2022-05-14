21fb5c070d732d685a3e0407415a324163fbce02
mahafuztechie <mahafuzaadeez5@gmail.com>
2022-05-14 12:01:34 +0530

    [mahafuz] added get all book api

---
+++ Bookstore_NhibernateApp/Bookstore_NhibernateApp/Controllers/BookController.cs
@@ -0,0 +1,24 @@
+﻿using Bookstore_NhibernateApp.Models;
+using NHibernate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Bookstore_NhibernateApp.Controllers
+{
+    public class BookController : ApiController
+    {
+        ISession session = OpenSessionNHibernate.OpenSession();
+        [Route("getBooks")]
+        public List<BookModel> GetBookList()
+        {
+            List<BookModel> Books = session.Query<BookModel>().ToList();
+            return Books;
+        }
+       
+      
+    }
+}

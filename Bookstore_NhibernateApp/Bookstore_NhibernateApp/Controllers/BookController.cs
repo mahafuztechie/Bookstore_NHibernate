c0a8f78cdc556cd321ef16cead29aadaa2b5ac11
mahafuztechie <mahafuzaadeez5@gmail.com>
2022-05-14 12:04:51 +0530

    [mahafuz] added updatebook api

--- Bookstore_NhibernateApp/Bookstore_NhibernateApp/Controllers/BookController.cs
+++ Bookstore_NhibernateApp/Bookstore_NhibernateApp/Controllers/BookController.cs
@@ -53,6 +53,46 @@ namespace Bookstore_NhibernateApp.Controllers
             return book;
         }
 
-       
+        [Route("update")]
+        [HttpPut]
+        public HttpResponseMessage Updatebook(BookModel bookModel)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var book = session.Get<BookModel>(bookModel.BookId);
+                    book.BookName = bookModel.BookName;
+                    book.AuthorName = bookModel.AuthorName;
+                    book.ActualPrice = bookModel.ActualPrice; ;
+                    book.Rating = bookModel.Rating;
+                    book.RatingCount = bookModel.RatingCount;
+                    book.Description = bookModel.Description;
+                    book.ActualPrice = bookModel.ActualPrice; ;
+                    book.DiscountPrice = bookModel.DiscountPrice;
+                    book.BookImage = bookModel.BookImage;
+                    book.BookQuantity = bookModel.BookQuantity;
+
+                    using (ITransaction transaction = session.BeginTransaction())
+                    {
+                        session.SaveOrUpdate(book);
+                        transaction.Commit();
+                    }
+                    return Request.CreateResponse(HttpStatusCode.OK, book);
+                }
+                else
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "Error !");
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+
+      
+
+
     }
 }

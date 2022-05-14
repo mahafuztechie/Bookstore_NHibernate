600d7ad2263223a001ec77ad6249eb3d6573a8a0
mahafuztechie <mahafuzaadeez5@gmail.com>
2022-05-14 12:02:41 +0530

    [mahafuz] added addbook api

--- Bookstore_NhibernateApp/Bookstore_NhibernateApp/Controllers/BookController.cs
+++ Bookstore_NhibernateApp/Bookstore_NhibernateApp/Controllers/BookController.cs
@@ -18,7 +18,34 @@ namespace Bookstore_NhibernateApp.Controllers
             List<BookModel> Books = session.Query<BookModel>().ToList();
             return Books;
         }
-       
-      
+
+        [Route("AddBook")]
+        [HttpPost]
+        public HttpResponseMessage AddNewBook(BookModel book)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    using (ITransaction transaction = session.BeginTransaction())
+                    {
+                        session.Save(book);
+                        transaction.Commit();
+                    }
+                    return Request.CreateResponse(HttpStatusCode.OK, book);
+                }
+                else
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "Error !");
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+
+     
+
     }
 }

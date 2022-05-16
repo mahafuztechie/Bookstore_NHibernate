29588bc16f3e415f084292f8f64d3991d232fd21
mahafuztechie <mahafuzaadeez5@gmail.com>
2022-05-16 12:29:45 +0530

    [mahafuz] added delete address api

--- Bookstore_NhibernateApp/Bookstore_NhibernateApp/Controllers/AddressController.cs
+++ Bookstore_NhibernateApp/Bookstore_NhibernateApp/Controllers/AddressController.cs
@@ -90,6 +90,33 @@ namespace Bookstore_NhibernateApp.Controllers
         }
 
 
-       
+        [HttpDelete]
+        public HttpResponseMessage DeleteAddress(int id, int userId)
+        {
+            try
+            {
+                var address = session.Get<AddressModel>(id);
+
+                if (address.UserId == userId)
+                {
+                    using (ITransaction transaction = session.BeginTransaction())
+                    {
+                        session.Delete(address);
+                        transaction.Commit();
+                        return Request.CreateResponse(HttpStatusCode.OK, "Address deleted Successfully");
+                    }
+
+                }
+                else
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Error !");
+                }
+            }
+            catch (Exception ex)
+            {
+
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
     }
 }

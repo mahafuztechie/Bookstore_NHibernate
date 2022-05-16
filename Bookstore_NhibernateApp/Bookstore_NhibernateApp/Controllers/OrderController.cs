2728dc0ff86dbcf7ab9c3812e08b9bca0cba58b0
mahafuztechie <mahafuzaadeez5@gmail.com>
2022-05-16 17:32:44 +0530

    [mahafuz] added add order api

---
+++ Bookstore_NhibernateApp/Bookstore_NhibernateApp/Controllers/OrderController.cs
@@ -0,0 +1,43 @@
+﻿using Bookstore_NhibernateApp.Models;
+using NHibernate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Bookstore_NhibernateApp.Controllers
+{
+    public class OrderController : ApiController
+    {
+        ISession session = OpenSessionNHibernate.OpenSession();
+ 
+        [Route("order")]
+        [HttpPost]
+        public HttpResponseMessage Order(OrderModel order)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    using (ITransaction transaction = session.BeginTransaction())
+                    {
+                        session.Save(order);
+                        transaction.Commit();
+                    }
+                    return Request.CreateResponse(HttpStatusCode.OK, order);
+                }
+                else
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "Error !");
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+      
+    }
+}

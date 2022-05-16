644ade4899ab69b9393752000a700c19f77391d3
mahafuztechie <mahafuzaadeez5@gmail.com>
2022-05-16 17:17:08 +0530

    [mahafuz] added ordermodel & mapping config

--- Bookstore_NhibernateApp/Bookstore_NhibernateApp/Models/OrderModel.cs
+++ Bookstore_NhibernateApp/Bookstore_NhibernateApp/Models/OrderModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,6 +8,21 @@ namespace Bookstore_NhibernateApp.Models
 {
     public class OrderModel
     {
+        public virtual int OrderId { get; set; }
+        public virtual int TotalPrice { get; set; }
 
+        public virtual int OrderBookQuantity { get; set; }
+
+        public virtual DateTime OrderDate { get; set; }
+
+        public virtual int UserId { get; set; }
+        [JsonIgnore]
+        public virtual UserModel UserModels { get; set; }
+        public virtual int BookId { get; set; }
+        [JsonIgnore]
+        public virtual BookModel BookModels { get; set; }
+        public virtual int AddressId { get; set; }
+        [JsonIgnore]
+        public virtual AddressModel AddressModel { get; set; }
     }
 }
